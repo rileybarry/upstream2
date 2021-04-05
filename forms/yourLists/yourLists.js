5d9591d42eb520cc653f37704f2bed3061aead01
Ethan Hall <lewisdecarolis@creighton.edu>
2021-04-05 16:39:43 -0500

    change to rdo

--- forms/yourLists/yourLists.js
+++ forms/yourLists/yourLists.js
@@ -1,14 +1,16 @@
 let listNames = ["new", "old"]
 
 yourLists.onshow=function(){
-    chkLists.clear()
+    rdoLists.clear()
     for (i = 0; i < listNames.length; i++)
-        chkLists.addItem(listNames[i])
+        rdoLists.addItem(listNames[i])
 }
 
 btnDeleteList.onclick=function(){
-    for (i = 0; i < chkLists.length; i++) {
-        if (chkLists.getValue(i) == 1)
-            console.log(i)
-    }
+    let sel = $("input[name=rdoLists]:checked").prop("value")
+    listNames.splice(listNames.indexOf(sel), 1)
+    
+    rdoLists.clear()
+    for (i = 0; i < listNames.length; i++)
+        rdoLists.addItem(listNames[i])
 }
